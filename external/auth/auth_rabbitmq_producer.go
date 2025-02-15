96467ad3e7ceee3f17b33a89993480bf319f3d36
gravitino <ngobinh0312@gmail.com>
2025-02-15 13:26:19 +0700

    remove rabbitmqSvc.connect

--- external/auth/auth_rabbitmq_producer.go
+++ external/auth/auth_rabbitmq_producer.go
@@ -1,7 +1,9 @@
 package auth
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/charitan-go/key-server/rabbitmq"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -20,11 +22,22 @@ func NewAuthRabbitmqProducer(rabbitmqSvc rabbitmq.RabbitmqService) AuthRabbitmqP
 }
 
 func (p *authRabbitmqProducerImpl) NotiGetPrivateKey() error {
-	ch, err := p.rabbitmqSvc.ConnectRabbitmq()
-	defer ch.Close()
+	amqpConnectionStr := fmt.Sprintf("amqp://%s:%s@message-broker:5672",
+		os.Getenv("MESSAGE_BROKER_USER"),
+		os.Getenv("MESSAGE_BROKER_PASSWORD"))
+	conn, err := amqp.Dial(amqpConnectionStr)
 	if err != nil {
-		log.Fatalf("Failed to open channel")
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+		return err
 	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+		return err
+	}
+	defer ch.Close()
 
 	// Declare a topic exchange (will create it if it doesn't exist)
 	exchangeName := "GET_PRIVATE_KEY"
